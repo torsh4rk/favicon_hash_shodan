b40007f217df65762e47cf609e02c7e2c9307602
torsh4rk <caduangelotti@gmail.com>
2021-08-16 22:23:46 -0300

    Update

--- README.md
+++ README.md
@@ -30,17 +30,26 @@ optional arguments:
 ```
 favicon_hash_shodan: python3 favicon_shodan.py --url=https://www.hackerone.com/favicon.ico
 
-[+] Target ===> www.hackerone.com
+[+] Favicon Domain Target ===> www.hackerone.com
+[+] View Results for Target www.hackerone.com (http.favicon.hash:595148549):
 
--> http.favicon.hash:595148549
--> Search in Shodan ===> https://www.shodan.io/search?query=http.favicon.hash%3A595148549+ip%3A104.16.99.52
+	-> Search in Shodan (Link 1) ===> https://www.shodan.io/search?query=http.favicon.hash%3A595148549 (Dork: http.favicon.hash:595148549)
+	-> Search in Shodan (Link 2) ===> https://www.shodan.io/search?query=http.favicon.hash%3A595148549+org%3Awww.hackerone.com (Dork: http.favicon.hash:595148549 + org:www.hackerone.com)
+	-> Search in Shodan (Link 3) ===> https://www.shodan.io/search?query=http.favicon.hash%3A595148549+ip%3A104.16.99.52 (Dork: http.favicon.hash:595148549 + ip:104.16.100.52)
 
-
-[!] Go to https://www.shodan.io/search?query=http.favicon.hash%3A595148549
-and view more results with the same favicon!
 
 
 ```
+
+## References
+
+```
+-> https://isc.sans.edu/forums/diary/Hunting+phishing+websites+with+favicon+hashes/27326/
+-> https://medium.com/@Asm0d3us/weaponizing-favicon-ico-for-bugbounties-osint-and-what-not-ace3c214e139
+-> https://infosecwriteups.com/using-shodan-better-way-b40f330e45f6
+
+```
+
 
 
 
--- favicon_shodan.py
+++ favicon_shodan.py
@@ -1,35 +1,78 @@
-import mmh3, base64, sys
-import requests, argparse, socket
+import mmh3, base64, sys, os
+import requests, argparse, socket, shodan
 from urllib.parse import urlparse
 from requests.exceptions import RequestException
 import urllib3
 
 urllib3.disable_warnings()
 
-parser = argparse.ArgumentParser(description="Search for a target by favicon")
-parser.add_argument("-u","--url", help="For a target URL. (Exemple: https://www.hackerone.com/favicon.ico)")
-args = parser.parse_args()
+def calcule_favicon_hash(URL, domain):
 
-URL = args.url
+    try:
+        req = requests.get(URL, verify=False)
+        resp = req.content
+        resp_encoded = base64.encodebytes(resp)
+        if req.status_code == 200 or req.status_code == 301 or req.status_code == 302:
+            hash_favicon = str(mmh3.hash(resp_encoded))
+            return hash_favicon
+        elif req.status_code == 404:
+            print("Favicon not found.")
+            return None
+    except (RequestException) as err:
+        print("\033[91mConnection Error. \nPlease, verify the target favicon URL or your connection.")
+        print(f"\033[91m\t-> More Error details: {err}\n")
+        sys.exit(-1)
+    except (KeyboardInterrupt):
+        print('\033[91mCtrl + C typed. Exiting...\n')
+        sys.exit(-1)
 
-if args.url == None:
-    print("[-] URL target is missing, try using -u <url> or --url <url>")
-    sys.exit()
-else:
-    domain = urlparse(URL).netloc
-    print(f"\n\033[94m\n[+] Target ===> {domain}\n")
+def print_shodan_links(URL, domain):
 
-try:
-    req = requests.get(URL, verify=False)
-    resp = req.content
-    resp_encoded = base64.encodebytes(resp)
-    if req.status_code == 200:
-        hash_favicon = str(mmh3.hash(resp_encoded))
-        query = f"{requests.utils.quote(f'http.favicon.hash:{hash_favicon}')}" + "+" + f"{requests.utils.quote(f'ip:{socket.gethostbyname(domain)}')}"
-        print(f'\033[92m-> http.favicon.hash:{hash_favicon}')
-        print(f"\033[92m-> Search in Shodan ===> https://www.shodan.io/search?query={query}\n")
-        print(f"\n\033[93m[!] Go to https://www.shodan.io/search?query/search?query={requests.utils.quote(f'http.favicon.hash:{hash_favicon}')}\nand view more results with the same favicon!\n")
+    hash_favicon = calcule_favicon_hash(URL, domain)
+    if hash_favicon != None:
+        query_1 = f"{requests.utils.quote(f'http.favicon.hash:{hash_favicon}')}"
+        query_2 = f"{requests.utils.quote(f'http.favicon.hash:{hash_favicon}')}" + "+" + f"{requests.utils.quote(f'org:{domain}')}"
+        query_3 = f"{requests.utils.quote(f'http.favicon.hash:{hash_favicon}')}" + "+" + f"{requests.utils.quote(f'ip:{socket.gethostbyname(domain)}')}"
+        print(f"\033[93m[+] View Results for Target {domain} (http.favicon.hash:{hash_favicon}):\n")
+        print(f'\033[92m\t-> Search in Shodan (Link 1) ===> https://www.shodan.io/search?query={query_1} (Dork: http.favicon.hash:{hash_favicon})')
+        print(f'\033[92m\t-> Search in Shodan (Link 2) ===> https://www.shodan.io/search?query={query_2} (Dork: http.favicon.hash:{hash_favicon} + org:{domain})')
+        print(f'\033[92m\t-> Search in Shodan (Link 3) ===> https://www.shodan.io/search?query={query_3} (Dork: http.favicon.hash:{hash_favicon} + ip:{socket.gethostbyname(domain)})')
     else:
-        pass
-except (RequestException, socket.gaierror) as err:
-    print(f"[-] One Error occurred: {err}")
+        sys.exit(1)
+
+
+def main():
+
+    # Arguments
+
+    parser = argparse.ArgumentParser(description="Search for a target by favicon")
+    parser.add_argument("-u","--url", help="For a favicon URL. (Exemple: https://www.hackerone.com/favicon.ico)")
+    parser.add_argument("-i","--input", help="For a favicon URL list.")
+    args = parser.parse_args()
+
+    URL = args.url
+    INPUT = args.input
+
+    # Validating the URL argument (args.url) and INPUT argument (args.input)
+
+    if args.url == None and args.input == None:
+        print("[-] URL and Wordlist target is missing, type -h for help usage")
+        sys.exit()
+    elif args.url != None and args.input == None:
+        if os.path.basename(urlparse(URL).path) == None or os.path.basename(urlparse(URL).path) != "favicon.ico":
+            print("[-] Favicon name is missing or it's wrong! Type -h or help for help usage.")
+            sys.exit()
+        else:
+            domain = urlparse(URL).netloc
+            print(f'\n\033[93m[+] Favicon Domain Target ===> {domain}')
+            print_shodan_links(URL, domain)
+    elif args.url == None and args.input != None:
+        print(f'[\n\033[93m+] Favicon URL Target (Wordlist) ===> {INPUT}')
+    else:
+        print(f'[-] URL and Wordlist can not be used together.')
+
+
+    # Checking the error connections, KeyboardInterrupt and Generating the Shodan Links
+
+if __name__ == "__main__":
+    main()
