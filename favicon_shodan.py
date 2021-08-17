e6477085fb52c37edde8828e7e51b282159a8619
torsh4rk <caduangelotti@gmail.com>
2021-08-17 00:35:37 -0300

    Update

--- favicon_shodan.py
+++ favicon_shodan.py
@@ -6,6 +6,9 @@ import urllib3
 
 urllib3.disable_warnings()
 
+shodan_cli_api_key = "" # YOUR API KEY
+
+
 def calcule_favicon_hash(URL, domain):
 
     try:
@@ -26,17 +29,23 @@ def calcule_favicon_hash(URL, domain):
         print('\033[91mCtrl + C typed. Exiting...\n')
         sys.exit(-1)
 
-def print_shodan_links(URL, domain):
+def print_shodan_links(URL, domain, VERBOSE):
 
     hash_favicon = calcule_favicon_hash(URL, domain)
     if hash_favicon != None:
         query_1 = f"{requests.utils.quote(f'http.favicon.hash:{hash_favicon}')}"
-        query_2 = f"{requests.utils.quote(f'http.favicon.hash:{hash_favicon}')}" + "+" + f"{requests.utils.quote(f'org:{domain}')}"
-        query_3 = f"{requests.utils.quote(f'http.favicon.hash:{hash_favicon}')}" + "+" + f"{requests.utils.quote(f'ip:{socket.gethostbyname(domain)}')}"
+        query_2 = f"{requests.utils.quote(f'http.favicon.hash:{hash_favicon}')}" + "+" + f"{requests.utils.quote(f'ip:{socket.gethostbyname(domain)}')}"
         print(f"\033[93m[+] View Results for Target {domain} (http.favicon.hash:{hash_favicon}):\n")
         print(f'\033[92m\t-> Search in Shodan (Link 1) ===> https://www.shodan.io/search?query={query_1} (Dork: http.favicon.hash:{hash_favicon})')
-        print(f'\033[92m\t-> Search in Shodan (Link 2) ===> https://www.shodan.io/search?query={query_2} (Dork: http.favicon.hash:{hash_favicon} + org:{domain})')
-        print(f'\033[92m\t-> Search in Shodan (Link 3) ===> https://www.shodan.io/search?query={query_3} (Dork: http.favicon.hash:{hash_favicon} + ip:{socket.gethostbyname(domain)})')
+        if VERBOSE == 1:
+            os.system(f"shodan search http.favicon.hash:{hash_favicon} " + "--fields ip_str,port --separator \" \" | awk '{print $1\":\"$2}'")
+        else:
+            pass
+        print(f'\033[92m\t-> Search in Shodan (Link 2) ===> https://www.shodan.io/search?query={query_2} (Dork: http.favicon.hash:{hash_favicon} + ip:{socket.gethostbyname(domain)})')
+        if VERBOSE == 1:
+            os.system(f"shodan search http.favicon.hash:{hash_favicon} + ip:{socket.gethostbyname(domain)} " + "--fields ip_str,port --separator \" \" | awk '{print $1\":\"$2}'")
+        else:
+            pass
     else:
         sys.exit(1)
 
@@ -48,10 +57,12 @@ def main():
     parser = argparse.ArgumentParser(description="Search for a target by favicon")
     parser.add_argument("-u","--url", help="For a favicon URL. (Exemple: https://www.hackerone.com/favicon.ico)")
     parser.add_argument("-i","--input", help="For a favicon URL list.")
+    parser.add_argument("-v","--verbose", type=int, choices=[0,1], default=0, help="Set a verbose value to use the shodan_cli (API KEY Required).")
     args = parser.parse_args()
 
     URL = args.url
     INPUT = args.input
+    VERBOSE = args.verbose
 
     # Validating the URL argument (args.url) and INPUT argument (args.input)
 
@@ -63,16 +74,19 @@ def main():
             print("[-] Favicon name is missing or it's wrong! Type -h or help for help usage.")
             sys.exit()
         else:
+            if shodan_cli_api_key != "":
+                print('\n')
+                os.system(f"shodan init {shodan_cli_api_key}")
             domain = urlparse(URL).netloc
             print(f'\n\033[93m[+] Favicon Domain Target ===> {domain}')
-            print_shodan_links(URL, domain)
+            print_shodan_links(URL, domain, VERBOSE)
     elif args.url == None and args.input != None:
+        if shodan_cli_api_key != "":
+            print('\n')
+            os.system(f"shodan init {shodan_cli_api_key}")
         print(f'[\n\033[93m+] Favicon URL Target (Wordlist) ===> {INPUT}')
     else:
         print(f'[-] URL and Wordlist can not be used together.')
-
-
-    # Checking the error connections, KeyboardInterrupt and Generating the Shodan Links
 
 if __name__ == "__main__":
     main()
